9e9b2e47bab9faddbe2bd8c85da5eee0487c5ca5
Karen <103889430+karen-nke@users.noreply.github.com>
2023-11-20 17:30:25 +0800

    Modify Homepage Section

--- Event/Public/index.php
+++ Event/Public/index.php
@@ -26,6 +26,7 @@ require_once('logic_controller.php');
 </head>
 
 <body>
+        <div class ="container">
        
                 <section class="hero">
                         <div class ="container">
@@ -79,6 +80,83 @@ require_once('logic_controller.php');
 
                 </section>
 
+                <div class ="section-container">
+                        <h2>Benefit of Program</h2>
+                        <div class="card-container">
+
+                  
+                                <div class="event-card">
+                                        <img src="Image/PointLevel_Icon.png" alt="Event Image">
+                                        <h2>Earn Points & Up Level</h2>
+                                        <p>Lorem ipsum dolor sit amet consecte tur adipiscing elit semper </p>
+                                                
+                                </div>
+
+              
+         
+                                <div class="event-card">
+                                        <img src="Image/Rewards_Icon.png" alt="Event Image">
+                                        <h2>Earn Badges</h2>
+                                        <p>Lorem ipsum dolor sit amet consecte tur adipiscing elit semper </p>
+                                       
+                                </div>
+
+  
+
+              
+                                <div class="event-card">
+                                        <img src="Image/Leaderboard_Icon2.png" alt="Event Image">
+                                        <h2>Leaderboard</h2>
+                                        <p>Lorem ipsum dolor sit amet consecte tur adipiscing elit semper </p>
+                                        
+                                </div>
+
+                               
+                        </div>
+                </div>
+
+                <div class ="section-container">
+                        <h2>Get Started</h2>
+                        <div class="card-container">
+
+                                <a href="soft_skills.php?skill=leadership">
+                                        <div class="event-card">
+                                                <img src="Image/Leadership_Icon.png" alt="Event Image">
+                                                <h2>Leadership</h2>
+                                                        
+                                        </div>
+
+                                </a>
+                                <a href="soft_skills.php?skill=communication">
+                                        <div class="event-card">
+                                                <img src="Image/Communication_Icon.png" alt="Event Image">
+                                                <h2>Communication</h2>
+                                                
+                                        </div>
+
+                                </a>
+
+                                <a href="soft_skills.php?skill=teamwork">
+                                        <div class="event-card">
+                                                <img src="Image/Teamwork_Icon.png" alt="Event Image">
+                                                <h2>Teamwork</h2>
+                                                
+                                        </div>
+
+                                </a>
+
+                     
+                               
+                               
+
+                              
+
+                               
+
+                               
+                        </div>
+                </div>
+        </div>
 
                 
                         
@@ -90,3 +168,63 @@ require_once('logic_controller.php');
 
 </html>
 
+<style>
+   .section-container{
+        display:flex;
+        flex-direction:column;
+        margin-top:50px;
+    }
+
+    .section-container .card-container {
+        display:flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+
+
+    .card-container .event-card {
+            width: 300px;
+            margin: 20px;
+            padding: 15px;
+            border: 1px solid #D89C60;
+            border-radius: 8px;
+            float: left;
+            background-color: white;
+     }
+
+    .event-card img{
+   
+    max-width: 100%;
+    height: auto;
+    border-radius: 6px;
+
+   
+    
+    }
+
+   .section-container .event-card h2{
+        color: #E87A00;
+        font-size: 18px;
+        font-weight: 700;
+        line-height: 42px;
+        text-align: center;
+    }
+
+    .section-container .event-card p{
+        
+        color: #045174;
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 30px;
+        text-align: center;
+    }
+    .section-container h2{
+        color: #E87A00;
+        font-size: 32px;
+        font-weight: 700;
+        line-height: 18px;
+        padding-bottom:25px;
+        margin:auto;
+    }
+
+</style>
