41fe3e6c162b70edfea47af1babbc272997e5a1f
Karen <103889430+karen-nke@users.noreply.github.com>
2023-12-05 12:07:20 +0800

    Remove Error

--- Event/Public/challenges_1.php
+++ Event/Public/challenges_1.php
@@ -1,6 +1,8 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
